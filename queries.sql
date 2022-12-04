cd3e1a0052d9a2bf089af5f243a7cd64d601e30a
qwe123adlet <118956738+qwe123adlet@users.noreply.github.com>
2022-12-04 10:26:33 +0600

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -20,24 +20,26 @@ INSERT INTO specializationtype VALUES(DEFAULT, 10, 'type10');
 DROP TABLE IF EXISTS employee CASCADE;
 
 CREATE TABLE employee (
-		id SERIAL PRIMARY KEY,
-		empname VARCHAR(100),
+        id SERIAL PRIMARY KEY,
+        empname VARCHAR(100),
         dateofbirth DATE,
         iin VARCHAR(12),
-		contact INT,
+        contact INT,
         departmentid VARCHAR(100),
         specializationid VARCHAR(100),
-		experience INT,
-		photo VARCHAR(100),
 		category VARCHAR(100),
-		price INT,
-		schedule VARCHAR(100),
-		education VARCHAR(100),
-		rating INT,
-		address VARCHAR(100),
-		FOREIGN KEY(specializationid) REFERENCES specializationtype(specializationid) ON UPDATE CASCADE ON DELETE CASCADE
+        experience INT,
+        photo VARCHAR(100),
+        schedulestart TIME,
+        scheduleend TIME,
+        price INT,
+        education VARCHAR(100),
+        rating INT,
+        address VARCHAR(100),
+        FOREIGN KEY(specializationid) REFERENCES specializationtype(specializationid) ON UPDATE CASCADE ON DELETE CASCADE
 );
-INSERT INTO specializationtype VALUES(DEFAULT, 'type1');
+INSERT INTO employee VALUES(DEFAULT, 'name', '2000-10-10', 'iin', '100', 'depid', '1', 'category', '500', 'photo', '09:00:00', '18:00:00', '22', 'education', '10', 'address');
+INSERT INTO employee VALUES(DEFAULT, 'name2', '2001-10-10', 'iin2', '200', 'depid2', '1', 'category2', '5002', 'photo2', '09:00:00', '18:00:00', '222', 'education2', '102', 'address2');
 
 DROP TABLE IF EXISTS patient CASCADE;
 
@@ -55,11 +57,32 @@ CREATE TABLE patient (
 );	
 
 DROP TABLE IF EXISTS appointment CASCADE;
+
 CREATE TABLE appointment(
-  id SERIAL PRIMARY KEY,
+  id SERIAL primary key,
   fullname varchar(100),
   app_date date,
   specializationid varchar(100),
   empname varchar(100),
-  contact varchar(100)
+  contact varchar(100),
+  app_start time,
+  app_end time
 );
+
+DROP TABLE IF EXISTS timeslots CASCADE;
+
+CREATE TABLE timeslots (
+		id SERIAL UNIQUE,
+		starttime TIME,
+		endtime TIME,
+		PRIMARY KEY(id)
+);	
+
+INSERT INTO timeslots VALUES(DEFAULT, '09:00:00', '10:00:00');
+INSERT INTO timeslots VALUES(DEFAULT, '10:00:00', '11:00:00');
+INSERT INTO timeslots VALUES(DEFAULT, '11:00:00', '12:00:00');
+INSERT INTO timeslots VALUES(DEFAULT, '12:00:00', '13:00:00');
+INSERT INTO timeslots VALUES(DEFAULT, '14:00:00', '15:00:00');
+INSERT INTO timeslots VALUES(DEFAULT, '15:00:00', '16:00:00');
+INSERT INTO timeslots VALUES(DEFAULT, '16:00:00', '17:00:00');
+INSERT INTO timeslots VALUES(DEFAULT, '17:00:00', '18:00:00');
